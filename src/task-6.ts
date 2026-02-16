a2d11a3b07a8088f7284e88725cd6da1e055cb13
Sergii Taran <serg.taran1970@gmail.com>
2026-02-16 10:40:07 +0200

    feat: add typed Promise return for getMessage function (task 7)

--- src/task-6.ts
+++ src/task-6.ts
@@ -19,6 +19,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-console.log(getFirstElement<number>([1, 2, 3]));
-console.log(getFirstElement<string>(["a", "b", "c"]));
-console.log(getFirstElement<boolean>([true, false, true]));
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
